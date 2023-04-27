4d9e66bee22a89e9d098f9b7fbf7ac5dc010e1b6
Dylanrealin <126303602+Dylanrealin@users.noreply.github.com>
2023-04-26 21:27:51 -0400

    added incomplete workshop

---
+++ index.js
@@ -0,0 +1,28 @@
+const timmy = {
+    prescription: "acetaminophen",
+    pricePerRefill: 25,
+    refills: 3,
+    subscription: false,
+    coupon: true,
+};
+
+const sarah = {
+    prescription: "diphenhydramine",
+    pricePerRefill: 50,
+    refills: 1,
+    subscription: true,
+    coupon: false,
+}
+
+const rocky = {
+    prescription: "phenylephrine",
+    pricePerRefill: 30,
+    refills: 5,
+    subscription: true,
+    coupon: true,
+}
+
+//construct a function that caculates a 25% discount after the refil total 
+//has been calculated
+
+const subDiscount = 
